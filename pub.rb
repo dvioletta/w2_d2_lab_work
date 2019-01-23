715a3d22bac1b76276edc5f5a06d5a3fa6e36405
Abigaila Ekierte <arekiert@gmail.com>
2019-01-23 11:34:15 +0000

    added 3 functions to pub

--- pub.rb
+++ pub.rb
@@ -3,11 +3,22 @@ class Pub
 attr_reader :name
 attr_writer :total_till
 
-  def initialize(name, total_till)
+  def initialize(name, till)
     @name = name
-    @total_till = total_till
+    @till = till
     @drinks = []
   end
 
+  def total_till()
+    return @till
+  end
+
+  def total_drinks()
+    return @drinks.length
+  end
+
+  def add_drink(drink)
+    @drinks << drink
+  end
 
 end
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -10,10 +10,27 @@ class TestPub < MiniTest::Test
 
   def setup
     @pub = Pub.new("Golden Lion", 120)
+    @drink_1 = Drink.new("Beer", 3, 2)
+    @drink_2 = Drink.new("Cider", 4, 5)
   end
 
   def test_get_pub_name()
     assert_equal("Golden Lion", @pub.name)
   end
+
+  def test_get_total_till()
+    assert_equal(120, @pub.total_till)
+  end
+
+  def test_total_drinks()
+    assert_equal(0, @pub.total_drinks)
+  end
+
+  def test_add_drink_to_pub()
+    @pub.add_drink(@drink_1)
+    assert_equal(1, @pub.total_drinks)
+  end
+
+  def test_get_drink_for_the_customer()
 
 end
